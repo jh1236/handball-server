a31f55a57cfc6250de0a80d5f0642a2431386135
healy <healy_jared@yahoo.com>
2024-06-18 01:29:23 +0800

    fix elo slightly more

--- test.py
+++ test.py
@@ -7,36 +7,40 @@ def regen_elo():
         c.execute("""DELETE FROM eloChange""")
         request = c.execute("""SELECT id, tournamentId FROM games""").fetchall()
         for game_id, tournament_id in request:
-            teams = c.execute("""
+            players = c.execute("""
 SELECT games.isRanked as ranked,
        games.winningTeam = teamId as myTeamWon,
        games.teamOne <> playerGameStats.teamId as isSecond,
        ROUND(1500.0 + coalesce((SELECT SUM(eloChange)
                        from eloChange
                        where eloChange.playerId = playerGameStats.playerid), 0), 2) as elo,
-       playerId as player
+       playerId as player,
+       (playerGameStats.roundsPlayed > 0) OR ((games.teamTwoScore + games.teamOneScore) = 0)
        FROM playerGameStats
          INNER JOIN games ON playerGameStats.gameId = games.id
 WHERE games.id = ? ORDER BY isSecond""", (game_id,)).fetchall()
-            if teams[0][0]:  # the game is unranked, so doing elo stuff is silly
-                elos = [0, 0]
-                team_sizes = [0, 0]
-                for i in teams:
-                    elos[i[2]] += i[3]
-                    team_sizes[i[2]] += 1
-                for i, v in enumerate(team_sizes):
-                    if not v:
-                        continue
-                    elos[i] /= v
-                print(f"{elos = }, {game_id = }")
-                for i in teams:
-                    win = i[1]
-                    my_team = i[2]
-                    player_id = i[4]
-                    elo_delta = calc_elo(elos[my_team], elos[not my_team], win)
-                    c.execute(
-                        """INSERT INTO eloChange(gameId, playerId, tournamentId, eloChange) VALUES (?, ?, ?, ?)""",
-                        (game_id, player_id, tournament_id, elo_delta))
+            if not players[0][0]:
+                continue # the game is unranked, so doing elo stuff is silly
+            elos = [0, 0]
+            team_sizes = [0, 0]
+            for i in players:
+                if not i[5]: continue
+                elos[i[2]] += i[3]
+                team_sizes[i[2]] += 1
+            for i, v in enumerate(team_sizes):
+                if not v:
+                    continue
+                elos[i] /= v
+            print(f"{elos = }, {game_id = }")
+            for i in players:
+                if not i[5]: continue
+                win = i[1]
+                my_team = i[2]
+                player_id = i[4]
+                elo_delta = calc_elo(elos[my_team], elos[not my_team], win)
+                c.execute(
+                    """INSERT INTO eloChange(gameId, playerId, tournamentId, eloChange) VALUES (?, ?, ?, ?)""",
+                    (game_id, player_id, tournament_id, elo_delta))
 
 
 if __name__ == '__main__':
--- utils/databaseManager.py
+++ utils/databaseManager.py
@@ -266,16 +266,16 @@ SELECT playerGameStats.id,
                     ORDER BY id desc
                     LIMIT 1)
        )                                                                                   as cardTime,
-       coalesce(SUM(gE.teamOneLeft = playerGameStats.playerId OR
+       coalesce(SUM((gE.teamOneLeft = playerGameStats.playerId OR
                     gE.teamOneRight = playerGameStats.playerId OR
                     gE.teamTwoLeft = playerGameStats.playerId OR 
-                    gE.teamTwoRight = playerGameStats.playerId) AND gE.eventType = 'Score', 0) as roundsPlayed,
+                    gE.teamTwoRight = playerGameStats.playerId) AND gE.eventType = 'Score'), 0) as roundsPlayed,
         --absolutely vile hack 
        coalesce(SUM(IIF(eventType = 'Start', games.teamOneScore + games.teamTwoScore, 0)), 0) -
-           coalesce(SUM(gE.teamOneLeft = playerGameStats.playerId OR
+           coalesce(SUM((gE.teamOneLeft = playerGameStats.playerId OR
                     gE.teamOneRight = playerGameStats.playerId OR
                     gE.teamTwoLeft = playerGameStats.playerId OR 
-                    gE.teamTwoRight = playerGameStats.playerId) AND gE.eventType = 'Score', 0) as roundsBenched
+                    gE.teamTwoRight = playerGameStats.playerId) AND gE.eventType = 'Score'), 0) as roundsBenched
                     
 
 FROM playerGameStats
