c2b186754a054523f6c5d577452e8c5d932928c3
Lach993 <lachlan@banks7.com>
2024-09-08 18:06:10 +0800

    Optimisations: auto minify, use waitress (p.s. such a cool name for it)

--- start.py
+++ start.py
@@ -2,27 +2,23 @@ import flask
 
 from database import db
 from website.website import init_api
+from waitress import serve
+from flask_minify import Minify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config['SECRET_KEY'] = 'secret!'
 
+Minify(app=app, html=True, js=True, cssless=True)
+
 db.init_app(app)
 init_api(app)
-
-cloudflare = False
-
-if cloudflare:
-    port = 443
-    ssl_context = ('resources/cert.pem', 'resources/key.pem')
-else:
-    port = 80
-    ssl_context = None
-    
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False, ssl_context=ssl_context)
+    print("Starting server")
+    serve(app, host='127.0.0.1', port=8080)
+    # app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False, ssl_context=ssl_context)
     # 5000: arbitrary port but one with higher value, lower ones might be reserved
