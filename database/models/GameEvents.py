227185276d6f8ccb05ff76d85b5f2a29f62957ea
healy <healy_jared@yahoo.com>
2024-10-07 20:57:40 +0800

    no explosion?

--- database/models/GameEvents.py
+++ database/models/GameEvents.py
@@ -92,16 +92,16 @@ class GameEvents(db.Model):
         return {
             "game": self.game.as_dict(),
             "event_type": self.event_type,
-            "team": self.team.as_dict(),
-            "player": self.player.as_dict(),
+            "team": self.team.as_dict() if self.team else None,
+            "player": self.player.as_dict() if self.player else None,
             "details": self.details,
             "notes": self.notes,
             "first_team_just_served": self.team_who_served_id == self.game.team_one_id,
             "side_served": self.side_served,
             "first_team_to_serve": self.team_to_serve_id == self.game.team_one_id,
             "side_to_serve": self.side_to_serve,
-            "team_one_left": self.team_one_left.as_dict(),
-            "team_one_right": self.team_one_right.as_dict(),
-            "team_two_left": self.team_two_left.as_dict(),
-            "team_two_right": self.team_two_right.as_dict(),
+            "team_one_left": self.team_one_left.as_dict() if self.team_one_left else None,
+            "team_one_right": self.team_one_right.as_dict() if self.team_one_right else None,
+            "team_two_left": self.team_two_left.as_dict() if self.team_two_left else None,
+            "team_two_right": self.team_two_right.as_dict() if self.team_two_right else None
         }
