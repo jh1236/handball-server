60df269b6006ccdbe4d3caaa9f165dd2487717cc
Lach993 <lachlan@banks7.com>
2024-10-13 00:39:59 +0800

    rewrote .bat with syntax that actually exists

--- start.bat
+++ start.bat
@@ -1,41 +1,40 @@
 @ECHO OFF
-@REM ECHO please open nginx.exe, after press any key to continue
-@REM pause   
-@REM keep exit code of start.py
+ECHO Please make sure nginx is running, routing port 80 to 8080
+timeout t/ 30
+
 set repeated_failure=0
 set github_token=PUT TOKEN HERE
 
+:BEGIN
 py start.py --no-debug
 set EXIT_CODE=%ERRORLEVEL%
 
-ECHO Server stopped with %EXIT_CODE%
+ECHO start.py exited with exit code %EXIT_CODE%
 
 
-if %EXIT_CODE% eq 1 (
+if %EXIT_CODE% == 1 (
     set /a repeated_failure+=1
 ) else (
     set repeated_failure=0
 )
 
-
-if %EXIT_CODE% eq 1 && %repeated_failure% lss 3 (
-    ECHO start.py is restarting... Attempt %repeated_failure%
-    goto :BEGIN
-) else if %EXIT_CODE% eq 1 (
-    ECHO start.py failed to start after %repeated_failure% attempts, waiting 5 minutes before pulling from github and retrying...
-    timeout /t 300
-    goto :FETCH_FROM_GITHUB
-) else if %EXIT_CODE% eq 2 (
-    ECHO start.py failed with exit code %EXIT_CODE%
-    goto :FETCH_FROM_GITHUB
-) else (
-    ECHO start.py exited with code %EXIT_CODE% - exiting...
-)
+if %EXIT_CODE% == 1 goto :RESTART
+if %EXIT_CODE% == 2 goto :UPDATE
+ECHO Server has been stopped.
 goto :END
 
-:FETCH_FROM_GITHUB
+:RESTART
+if %repeated_failure% lss 3 (
+    ECHO start.py is restarting... Attempt %repeated_failure%
+) else (
+    ECHO start.py failed to start after %repeated_failure% attempts, waiting 5 minutes before pulling from github and retrying...
+    timeout /t 300
+    goto :UPDATE
+)
+goto :BEGIN
+
+:UPDATE
 ECHO Pulling from github...
-@REM IT IS IMPERATIVE THAT THE FOLLOWING LINE IS NEVER LEAKED BECAUSE I CANNOT BE FUCKED USING SYSTEM VARIABLES
 git pull https://%github_token%@github.com/jh1236/matchmaking
 ECHO Retrying start.py... Attempt %repeated_failure%
 goto :BEGIN
